d6d76a5cca72aa465e4f9632156a3efec4a3ac02
edouard06 <edouarrddd@gmail.com>
2025-04-16 18:22:59 +0200

    upgrade angular form 17.3

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,44 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from '../shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './features/auth/auth.module';
-import { SharedModule } from '../shared/shared.module';
 import { ArticlesModule } from './features/articles/articles.modules';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ListComponent } from './pages/theme-list/theme-list.component';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { ThemeListComponent } from './pages/theme-list/theme-list.component';
-import { ThemeCardComponent } from '../shared/components/theme-card/theme-card.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     CoreModule,
     AuthModule,
     SharedModule,
     ArticlesModule,
-    MatButtonModule,
   ],
   declarations: [
     AppComponent,
     HomeComponent,
     ProfileComponent,
-    ThemeListComponent,
-    ThemeCardComponent,
+    ListComponent,
   ],
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
 })
--- front/src/shared/shared.module.ts
+++ front/src/shared/shared.module.ts
@@ -1,20 +1,27 @@
+/**
+ * The `SharedModule` is an Angular module that encapsulates commonly used components, directives, and Angular Material modules
+ * which can be shared across multiple feature modules in the application. This helps to avoid redundancy and promotes reusability.
+ *
+ * @module SharedModule
+ */
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-// Angular Material
+// Angular Material modules
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
-import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatLegacySnackBarModule as MatSnackBarModule } from '@angular/material/legacy-snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-import { EllipsisModule } from 'ngx-ellipsis'; // Ensure this path is correct and matches the library's documentation
+// Third-party modules
+import { EllipsisModule } from 'ngx-ellipsis'; // Ensure this is the correct package and properly installed
 
 // Components
 import { HeaderComponent } from './components/header/header.component';
@@ -31,6 +38,7 @@ import { ThemeCardComponent } from './components/theme-card/theme-card.component
     FormsModule,
     ReactiveFormsModule,
     EllipsisModule,
+
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
@@ -43,11 +51,13 @@ import { ThemeCardComponent } from './components/theme-card/theme-card.component
   ],
   exports: [
     CommonModule,
+    RouterModule,
     FormsModule,
     ReactiveFormsModule,
+    EllipsisModule,
     HeaderComponent,
     ThemeCardComponent,
-    EllipsisModule,
+
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
